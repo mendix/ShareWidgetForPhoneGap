9d198b1303e6098a29385392fe556175f7101dfa
Jelte Lagendijk <jelte.lagendijk@mendix.com>
2016-11-16 16:44:25 +0100

    Replace obsolete code with abstract lib

--- src/ShareWidgetForPhoneGap/widget/ShareWidgetForPhoneGap.js
+++ src/ShareWidgetForPhoneGap/widget/ShareWidgetForPhoneGap.js
@@ -1,79 +1,21 @@
 define([
     "dojo/_base/declare",
-    "mxui/widget/_WidgetBase",
-    "dojo/_base/lang",
-    "dojo/query",
-    "dojo/_base/array",
-    "dojo/NodeList-traverse"
-], function(declare, _WidgetBase, lang, query, array) {
+    "ShareWidgetForPhoneGap/lib/AbstractPhoneGapWidget/widget"
+], function(declare, AbstractPhoneGapWidget) {
     "use strict";
 
-    return declare("ShareWidgetForPhoneGap.widget.ShareWidgetForPhoneGap", _WidgetBase, {
+    return declare("ShareWidgetForPhoneGap.widget.ShareWidgetForPhoneGap", [AbstractPhoneGapWidget], {
 
         // Set in modeler
-        elementClass: "",
-        elementName: "",
         messageAttr: "",
         linkAttr: "",
         subjectAttr: "",
 
-        // internal variables.
-        _setup: false,
-        _obj: null,
+        // Overwriting Abstract widget
+        phoneGapPluginName: "socialsharing",
+        pluginNotFoundError: "Unable to detect Phonegap/Social sharing functionality.",
 
-        update: function(obj, callback) {
-            this._obj = obj;
-
-            if (this.elementClass === "" && this.elementName === "") {
-                logger.warn(this.id + ".update: No element/class is set in the modeler");
-                return;
-            }
-
-            if (!this._setup) {
-                this._setupWidget(callback);
-            } else {
-                mendix.lang.nullExec(callback);
-            }
-        },
-
-        _setupWidget: function(callback) {
-            logger.debug(this.id + "._setupWidget");
-            this._setup = true;
-
-            this._setElementEventHandler();
-
-            mendix.lang.nullExec(callback);
-        },
-
-        _setElementEventHandler: function() {
-            logger.debug(this.id + "._setElementEventHandler");
-            var className = this.elementClass || ".mx-name-" + this.elementName,
-                parentNode = query(this.domNode).parent(),
-                targetElements = parentNode.children(className).first();
-
-            if (targetElements.length === 0) {
-                logger.warn(this.id + "._setElementEventHandler: Can't find element with class " + className + ", quiting");
-                return;
-            }
-
-            array.forEach(targetElements, lang.hitch(this, function(el, i) {
-                this._setupEvents(el, className);
-            }));
-        },
-
-        _setupEvents: function(element, className) {
-            logger.debug(this.id + "._setupEvents " + className);
-            // Attach only one event to dropdown list.
-            this.connect(element, "click", lang.hitch(this, function(evt) {
-                if (!window.plugins || !window.plugins.socialsharing) {
-                    mx.ui.error("Unable to detect Phonegap/Social sharing functionality.");
-                    return;
-                }
-                this._share();
-            }));
-        },
-
-        _share: function() {
+        _onClickAction: function() {
             var msg = this._obj.get(this.messageAttr),
                 subject = null,
                 link = null;
@@ -91,4 +33,4 @@ define([
 });
 
 // Compatibility with older mendix versions.
-require(["ShareWidgetForPhoneGap/widget/ShareWidgetForPhoneGap"], function() {});
+require(["ShareWidgetForPhoneGap/widget/ShareWidgetForPhoneGap"]);
